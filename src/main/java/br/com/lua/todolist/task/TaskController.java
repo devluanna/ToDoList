f7abb786ffa92c735929337db9567ab3b80d246f
Luana Alves Nogueira <luanagcontato@gmail.com>
2023-10-13 21:39:19 -0300

    Criação e atualização de Tasks

--- src/main/java/br/com/lua/todolist/task/TaskController.java
+++ src/main/java/br/com/lua/todolist/task/TaskController.java
@@ -1,15 +1,21 @@
 package br.com.lua.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.lua.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -19,7 +25,7 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @PostMapping("/add")
+    @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
         var idUser = request.getAttribute("idUser");
@@ -39,6 +45,26 @@ public class TaskController {
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+       var idUser = request.getAttribute("idUser");
+       var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+       return tasks;
+      
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
+    }
+
     
     
 }
